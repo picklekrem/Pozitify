210e0fc7b890344416ea3e8c5f3e753f7d49de5e
Ekrem <ekrem@3pounds.io>
2021-06-01 14:12:01 +0300

    First commit, good commit :)

---
+++ Pozitify/Pozitify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pozitify
+//
+//  Created by Ekrem Özkaraca on 1.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
