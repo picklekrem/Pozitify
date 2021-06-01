16f784a32cc3b19f95a72ff0d6db3c37859c4b99
onur.basdas <onur.basdas@3pounds.io>
2021-06-01 15:14:55 +0300

    tab bar update

--- Pozitify/Pozitify/AppDelegate.swift
+++ Pozitify/Pozitify/AppDelegate.swift
@@ -14,9 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        UITabBar.appearance().barTintColor = .green
-        UITabBar.appearance().tintColor = .white
-        UITabBar.appearance().unselectedItemTintColor = .blue
+        UITabBar.appearance().barTintColor = .black
+        UITabBar.appearance().tintColor = UIColor(named: "MainColor")
+        UITabBar.appearance().unselectedItemTintColor = .white
         return true
     }
 
